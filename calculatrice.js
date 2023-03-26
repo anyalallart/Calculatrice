310ab31af95357d4437f0a2b0a593aa01f270e71
anyalallart <anya.lallart@student.junia.com>
2023-03-26 17:09:40 +0200

    partie 3

--- calculatrice.js
+++ calculatrice.js
@@ -1,22 +1,30 @@
-function efface() {
-    document.getElementById("output").innerHTML = "0";
-}
+class BaseCalculator {
+    constructor (){
+        this.out = document.getElementById("output");
+        console.log (this.out);
+    }
 
-function clique(carac){
-    let v = document.getElementById("output");
-    if(carac == "r") carac = ")";
-    if(carac == "l") carac = "(";
-
-    if (v.innerHTML == "0") v.innerHTML = carac;
-    else v.innerHTML += carac;
-}
-
-function calcul(){
-    try {
-        let equa = eval(document.getElementById("output").innerHTML);
-        document.getElementById('output_solve').innerHTML = document.getElementById('output').innerHTML + " = " + equa;
-        document.getElementById('output').innerHTML = "0";
-    } catch (error) {
-        document.getElementById('output').innerHTML = "Error";
+    efface() {
+        this.out.innerHTML = "0";
+    }
+    
+    clique(carac){
+        if(carac == "r") carac = ")";
+        if(carac == "l") carac = "(";
+    
+        if (this.out.innerHTML == "0") this.out.innerHTML = carac;
+        else this.out.innerHTML += carac;
+    }
+    
+    calcul(){
+        try {
+            let equa = eval(this.out.innerHTML);
+            document.getElementById('output_solve').innerHTML = this.out.innerHTML + " = " + equa;
+            this.out.innerHTML = "0";
+        } catch (error) {
+            this.out.innerHTML = "Error";
+        }
     }
 }
+
+let baseCalculator = new BaseCalculator();
