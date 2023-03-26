a105d1cc41512de3ca622aa05f77dd7b4a32d8fb
anyalallart <anya.lallart@student.junia.com>
2023-03-26 19:20:45 +0200

    partie 4, bonus

--- calculatrice.js
+++ calculatrice.js
@@ -1,17 +1,21 @@
 class BaseCalculator {
     constructor (){
         this.out = document.getElementById("output");
-        console.log (this.out);
+        this.curentCalcul = [];
+        this.totalCalcul = [];
     }
 
     efface() {
         this.out.innerHTML = "0";
+        this.curentCalcul = [];
     }
     
     clique(carac){
         if(carac == "r") carac = ")";
         if(carac == "l") carac = "(";
     
+        this.curentCalcul.push(carac);
+
         if (this.out.innerHTML == "0") this.out.innerHTML = carac;
         else this.out.innerHTML += carac;
     }
@@ -19,12 +23,35 @@ class BaseCalculator {
     calcul(){
         try {
             let equa = eval(this.out.innerHTML);
+            this.totalCalcul.push(this.curentCalcul);
             document.getElementById('output_solve').innerHTML = this.out.innerHTML + " = " + equa;
-            this.out.innerHTML = "0";
+            this.efface();
         } catch (error) {
             this.out.innerHTML = "Error";
         }
     }
+
+    retour(){
+        try {
+            if (this.curentCalcul.length == 0){
+                this.out.innerHTML = "";
+                this.curentCalcul = this.totalCalcul[this.totalCalcul.length - 1];
+                this.curentCalcul.forEach(e=>{
+                    this.out.innerHTML += e;
+                })
+            }
+            else {
+                this.out.innerHTML = "";
+                this.curentCalcul.pop();
+                this.curentCalcul.forEach(e=>{
+                    this.out.innerHTML += e;
+                })
+            }
+        } catch (error) {
+            this.out.innerHTML = "Error";
+        }
+    }
+
 }
 
 let baseCalculator = new BaseCalculator();
