ffb3eb412596cb58c225d75821802d9a50584ffd
anyalallart <anya.lallart@student.junia.com>
2023-03-27 10:32:57 +0200

    commentaire sur le code

--- calculatrice.js
+++ calculatrice.js
@@ -1,16 +1,18 @@
 class BaseCalculator {
     constructor (){
         this.out = document.getElementById("output");
-        this.curentCalcul = [];
-        this.totalCalcul = [];
+        this.curentCalcul = []; // calcul en cours
+        this.totalCalcul = []; // calculs effectués
     }
 
     efface() {
+        // fonction qui efface le calcul en cours
         this.out.innerHTML = "0";
         this.curentCalcul = [];
     }
     
     clique(carac){
+        // fonction qui affiche et ajoute au calcul le caractère choisi
         if(carac == "r") carac = ")";
         if(carac == "l") carac = "(";
     
@@ -21,6 +23,7 @@ class BaseCalculator {
     }
     
     calcul(){
+        // fonction qui renvoie le calcul effectué
         try {
             let equa = eval(this.out.innerHTML);
             this.totalCalcul.push(this.curentCalcul);
@@ -32,8 +35,9 @@ class BaseCalculator {
     }
 
     retour(){
+        // fonction de la touche retour
         try {
-            if (this.curentCalcul.length == 0){
+            if (this.curentCalcul.length == 0){ // si aucun calcul n'est en cours, on affiche le précédent effectué
                 if (this.totalCalcul.length == 0) this.out.innerHTML = "0";
                 else this.out.innerHTML = "";
                 this.curentCalcul = this.totalCalcul[this.totalCalcul.length - 1];
